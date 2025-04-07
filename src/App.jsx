286da2e2def77f7ee037a3a7cf538b9f7f454fd1
yasser ramdani <139624089+IkhMoh@users.noreply.github.com>
2025-04-07 17:56:06 +0100

    adding th product in products

--- src/App.jsx
+++ src/App.jsx
@@ -1,54 +1,18 @@
 import "./App.css";
-import axios from "axios";
-import Product from "./components/Product";
+import Products from "./components/Products";
 import NavBar from "./components/NavBar";
 import { createTheme, ThemeProvider } from "@mui/material/styles";
-import { useEffect, useState } from "react";
 const theme = createTheme({
   typography: {
     fontFamily: "Rubik",
   },
 });
-let cancelGet = null;
 function App() {
-  const [productss, setProducts] = useState(["kk"]);
-  useEffect(() => {
-    axios
-      .get("https://dummyjson.com/products", {
-        cancelToken: new axios.CancelToken((c) => {
-          cancelGet = c;
-        }),
-      })
-      .then(function (response) {
-        setProducts(response.data.products);
-      })
-      .catch(function (error) {
-        console.log(error);
-      });
-    return () => {
-      console.log("cansle");
-      cancelGet();
-    };
-  }, []);
-  // console.log(products);
   return (
     <div>
       <ThemeProvider theme={theme}>
         <NavBar />
-        {/* <Product /> */}
-        <div className="flex justify-center mt-12 gap-4 flex-wrap">
-          {productss.map((product) => (
-            <div className="border-2 pb-36 p-8">
-              <img
-                className=""
-                key={product.id}
-                width="250"
-                src={product.thumbnail}
-              />
-              <div>hello</div>
-            </div>
-          ))}
-        </div>
+        <Products />
       </ThemeProvider>
     </div>
   );
---
+++ src/components/Products.jsx
@@ -0,0 +1,37 @@
+import React from "react";
+import Grid from "@mui/material/Grid2";
+import axios from "axios";
+import Product from "./Product";
+import Container from "@mui/material/Container";
+import { useState, useEffect } from "react";
+let cancelGet = null;
+const Products = () => {
+  const [productsData, setProductsData] = useState([]);
+  useEffect(() => {
+    axios
+      .get("https://dummyjson.com/products", {
+        cancelToken: new axios.CancelToken((c) => {
+          cancelGet = c;
+        }),
+      })
+      .then((response) => {
+        setProductsData(response.data.products);
+      })
+      .catch((error) => {});
+    return () => {
+      cancelGet();
+    };
+  }, []);
+
+  return (
+    <main>
+      <Container maxWidth="xl">
+        {productsData.map((product) => (
+          <Product key={product.id} product={product} />
+        ))}
+      </Container>
+    </main>
+  );
+};
+
+export default Products;
